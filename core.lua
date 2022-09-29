fe082f0558944fbbe63195ba09a9b0a2a885604d
Kozoaku <93962386+Kozoaku@users.noreply.github.com>
2022-09-29 09:44:33 -0400

    Init (#1)
    Initial addon release and test of the workflow.

---
+++ core.lua
@@ -0,0 +1,73 @@
+local select = select;
+
+local name = select(1, ...);
+
+local LibStub         = LibStub;
+local AceAddon        = LibStub:GetLibrary("AceAddon-3.0"); ---@type AceAddon-3.0
+local AceLocale       = LibStub:GetLibrary("AceLocale-3.0"); ---@type AceLocale-3.0
+local AceDB           = LibStub:GetLibrary("AceDB-3.0"); ---@type AceDB-3.0
+local AceDBOptions    = LibStub:GetLibrary("AceDBOptions-3.0");
+local AceConfig       = LibStub:GetLibrary("AceConfig-3.0"); ---@type AceConfig-3.0
+local AceConfigDialog = LibStub:GetLibrary("AceConfigDialog-3.0");
+
+
+---@class HelloWorld: AceAddon, AceConsole-3.0, AceEvent-3.0
+local HelloWorld = AceAddon:NewAddon(name, "AceConsole-3.0", "AceEvent-3.0");
+
+---@type HelloWorldLocale
+local L = AceLocale:GetLocale(name);
+
+function HelloWorld:OnInitialize()
+    local defaults = {
+        profile = {
+            general = {
+                message = L['Hello World'],
+            }
+        }
+    }
+    self.db = AceDB:New(name.."DB", defaults, true);
+
+    local options = {
+        name = name,
+        handler = HelloWorld,
+        type = 'group',
+        args = {
+            general = {
+                name = GENERAL_LABEL,
+                type = 'group',
+                args = {
+                    message = {
+                        name = L['Message'],
+                        desc = L['The message to be displayed'],
+                        type = 'input',
+                        set = 'SetMessage',
+                        get = 'GetMessage',
+                    }
+                }
+            },
+            profiles = AceDBOptions:GetOptionsTable(self.db);
+        }
+    }
+    AceConfig:RegisterOptionsTable(name, options, {"hw", "hello", "helloWorld"});
+
+    self.frames = {};
+    self.frames.options = AceConfigDialog:AddToBlizOptions(name.."_Options", L['Hello World'], nil, options.args.general);
+    self.frames.profiles = AceConfigDialog:AddToBlizOptions(name.."_Profiles", L['Profiles'], name.."_Options", options.args.profiles);
+end
+
+function HelloWorld:OnEnable()
+    if self.frames.bar == nil then
+        self.frames.bar = CreateFrame("Frame", name.."BarFrame", UIParent);
+    end
+end
+
+function HelloWorld:OnDisable()
+end
+
+function HelloWorld:SetMessage(info, value)
+    self.db.profile.message = value;
+end
+
+function HelloWorld:GetMessage(info)
+    return self.db.profile.message;
+end
